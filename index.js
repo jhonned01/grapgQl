a81932d389b22145028afeac49fd140882638094
jhonned01 <jhonned01@gmail.com>
2020-11-18 13:59:29 -0500

    agregando custom types

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
 'use strict'
 
-const express= require('express')
+const express = require('express')
+const {  buildSchema } = require('graphql')
+const { graphqlHTTP } = require('express-graphql')
+const {readFileSync}= require('fs')
+const {join}= require('path')
 
-const { graphql, buildSchema } = require('graphql')
-
-const  { graphqlHTTP}= require('express-graphql')
-
-
-
+const resolvers = require('./lib/resolvers')
 // Construye un esquema, usando el lenguaje de esquema GraphQL
-const schema =buildSchema (`
-    type Query {
-        hello: String
-        saludo: String
-    }
-`)
+const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'),'utf-8'))
 // La raíz proporciona una función de resolución para cada punto final de la API
-const root={
-    hello:()=>{
-        return 'hola mundo'
-    },
-    saludo:()=>{
-        return 'hola a todos'
-    }
+const root = {
+
 }
 
-
-
-//Ejecutar el query hello terminal
+// Ejecutar el query hello terminal
 // graphql(schema, '{saludo}', resolvers).then((data)=>{
 //     console.log(data);
 // })
 
 const app = express()
-app.use(`/api`,graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
+app.use('/api', graphqlHTTP({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
 
-})) 
+}))
 
 app.listen(4000)
-console.log('Running a GraphQL API server at http://localhost:4000/api');
+console.log('Running a GraphQL API server at http://localhost:4000/api')
